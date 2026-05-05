353e6a5b0d2b4bbc0bf78de7c67a75f92b9d56fc
utee <utibeabasiekong2@gmail.com>
2026-05-05 21:34:14 +0100

    chore: update formula for v0.1.2

--- Formula/autocom.rb
+++ Formula/autocom.rb
@@ -1,8 +1,8 @@
 class Autocom < Formula
   desc "Lightweight git auto-commit watcher"
   homepage "https://github.com/xlaez/autocom"
-  url "https://github.com/xlaez/autocom/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "943d3a986c0e37b445a85d72674f1a30c76b0e1123714b97298eb090d8d4b528"
+  url "https://github.com/xlaez/autocom/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "12fb1a47347f48a4afc2711b9d0b97a84cad4c4906ef3f8cda40db22ea25f1b8"
   license "MIT"
 
   depends_on "ripgrep"
